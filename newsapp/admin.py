a3d9599548804767040bfbb149e2c154f4c2bc40
Francky <gwadafrank@gmail.com>
2021-06-30 10:35:33 +0300

    add models to admin and add tests - add models to admin - add models class -migrations

--- newsapp/admin.py
+++ newsapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Editor,Article,Tags
+
+admin.site.register(Editor)
+admin.site.register(Article)
+admin.site.register(Tags)
--- newsapp/models.py
+++ newsapp/models.py
@@ -8,4 +8,25 @@ class Editor(models.Model):
 
     def __str__(self):
         return self.first_name
+
+    class Meta:
+        ordering = ['first_name']
+
+class Tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor, on_delete=models.CASCADE,)
+    tags = models.ManyToManyField(Tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
         
